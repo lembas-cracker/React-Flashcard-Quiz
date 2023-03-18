c381c8a120af21a49f6c6ef41f3480565c6a261f
Ksenia Agalakova <agalaksu111@gmail.com>
2023-03-18 18:58:12 +0300

    React Setup and boilerplate

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+import './app.css'
+
+function App() {
+  const [flashcards, setFlashcards] = useState(sampleFlashcards)
+  return (
+    <FlashcardList flashcards={flashcards} />
+  );
+}
+
+const sampleFlashcards = [
+  {
+    id: 1,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question 2?',
+    answer: '[Answer]',
+    options: [
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  }
+]
+
+
+
+export default App;
