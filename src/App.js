b3fc50eef8715391350167c97b27c60debb9ac59
Ksenia Agalakova <agalaksu111@gmail.com>
2023-03-20 22:58:54 +0300

    Added Open Trivia DB API calling function with axios, height sizing function, css grid and some card styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(sampleFlashcards)
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(answer => decodeString(answer)),
+            answer
+          ]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)   //randomly sorts the order of multiple choice list of options so the correct answer is never on the same place
+          }
+        }))
+        console.log(res.data)
+      })
+  }, [])
+
+
+  //decoding HTML-encoded punctuation marks etc.
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+
   return (
-    <FlashcardList flashcards={flashcards} />
+    <div className='container'>
+      <FlashcardList flashcards={flashcards} />
+    </div>
   );
 }
 
